8c84f7bd66dcdbdc99029341c99bc30b7f5867c1
rabia23 <rabia.iftikhar@arbisoft.com>
2016-05-17 17:08:43 +0500

    apply date filter in report.

--- src/app/reports/reports-controller.js
+++ src/app/reports/reports-controller.js
@@ -3,13 +3,36 @@
 
   .controller( 'ReportsCtrl', function ReportsCtrl( $scope, $rootScope, flashService, ReportsApi) {
 
+    $scope.today = new Date();
+    $scope.show_loading = true;
+
     var vm = this;
+    vm.resetDates = resetDates;
     vm.generate_report = generate_report;
 
+    vm.resetDates();
     vm.generate_report();
 
-    function generate_report(){
-      ReportsApi.report().$promise.then(function(data){
+    function resetDates(){
+      $scope.date = {
+        startDate: moment().subtract(1, "days"),
+        endDate: moment()
+      };
+    }
+
+    $scope.datePickerOption = {
+      eventHandlers: {
+        'apply.daterangepicker': function(ev, picker){
+          $scope.show_loading = true;
+          vm.generate_report(ev.model.startDate._i, ev.model.endDate._i);
+        }
+      },
+      opens: "left"
+    };
+
+    function generate_report(start_date, end_date){
+      ReportsApi.report(start_date, end_date).$promise.then(function(data){
+        $scope.show_loading = false;
         if(data.success){
           $scope.complaint_view = data.response.complaint_view;
           $scope.concerns = data.response.concerns;
