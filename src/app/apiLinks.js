423100efa76e13df8c3bf225ff763f29547a6f9b
rabia23 <rabia.iftikhar@arbisoft.com>
2016-08-10 12:14:15 +0500

    change web socket url.

--- src/app/apiLinks.js
+++ src/app/apiLinks.js
@@ -10,7 +10,7 @@ angular.module('livefeed.api_links', [])
       analytics: function(){
         //return $window.ga('create', 'UA-75485796-1', 'none');
       },
-      socket: "wss://ginsoy.sentimeter.io:5679/"
+      socket: "wss://apimcdonaldsqatar.sentimeter.io:5679/test1/"
     }
     // link: {
     //   api: "https://apimcdonalds.sentimeter.io/api/:endpoint",
