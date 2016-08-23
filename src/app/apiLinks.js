b7b52f5fba0f8f6be2b7b6a0bad46cc91d039231
rabia23 <rabia.iftikhar@arbisoft.com>
2016-08-23 15:18:00 +0500

    changed apilinks url

--- src/app/apiLinks.js
+++ src/app/apiLinks.js
@@ -10,7 +10,7 @@ angular.module('livefeed.api_links', [])
       analytics: function(){
         //return $window.ga('create', 'UA-75485796-1', 'none');
       },
-      socket: "wss://stagingmcdonalds.sentimeter.io:5679/"
+      socket: "wss://mcdonalds.sentimeter.io:5679/"
     }
     // link: {
     //   api: "https://apimcdonalds.sentimeter.io/api/:endpoint",
